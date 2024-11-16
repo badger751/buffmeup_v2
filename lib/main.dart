66b8db70e4a77b35c98affcab05f037b761a3e07
badger751 <achintyamanstar@gmail.com>
2024-11-16 07:41:58 +0530

    first feature commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
-import 'package:buffmeup_v2/firebase_options.dart';
+
 import 'package:buffmeup_v2/views/screens/login.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 
 
 
-Future<void> main() async{
 
-WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform
-);
-runApp(const MyApp());
+
+void main(){
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -41,7 +38,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPage(),
+      home: const LoginPage()
     );
   }
 }
